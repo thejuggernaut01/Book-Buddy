bd25e52e4ee5c376b18784aa6f31879d22f73365
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-11-02 11:32:53 +0100

    book model

---
+++ models/book.js
@@ -0,0 +1,34 @@
+const { getDB } = require("../utils/database");
+
+class Book {
+  constructor(
+    title,
+    description,
+    author,
+    publicationDate,
+    rating,
+    image,
+    printLength,
+    language,
+    readingAge,
+    isbn13
+  ) {
+    this.title = title;
+    this.description = description;
+    this.author = author;
+    this.publicationDate = publicationDate;
+    this.rating = rating;
+    this.image = image;
+    this.printLength = printLength;
+    this.language = language;
+    this.readingAge = readingAge;
+    this.isbn13 = isbn13;
+  }
+
+  save() {
+    let db = getDB();
+    return db;
+  }
+}
+
+module.exports = Book;
