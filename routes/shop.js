1a171e99b6131367624de0ae383ad1239971b6c2
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-11-10 07:09:32 +0100

    added auth to shop.js & user.js routes; added logout route in auth.js

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const shopController = require("../controllers/shop");
+const isAuthorized = require("../middleware/isAuthorized");
 
 router.get("/", shopController.getHome);
 
 router.get("/books", shopController.getAllBooks);
 
-router.get("/favorite", shopController.getFavorite);
+router.get("/favorite", isAuthorized, shopController.getFavorite);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/user");
+const isAuthorized = require("../middleware/isAuthorized");
 
-router.get("/add-book", userController.getAddBook);
+router.get("/add-book", isAuthorized, userController.getAddBook);
 
-router.get("/my-books", userController.getMyBooks);
+router.get("/my-books", isAuthorized, userController.getMyBooks);
 
 module.exports = router;
