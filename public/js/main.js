4abeb008bce6c0a1715c0cdd5fce3aae94e40e91
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-11-12 21:27:28 +0100

    frvftf

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,9 @@ const backdrop = document.querySelector(".backdrop");
 const sideDrawer = document.querySelector(".mobile-nav");
 const menuToggle = document.querySelector("#side-menu-toggle");
 
+const dropContainer = document.getElementById("dropContainer");
+const uploadFile = document.getElementById("uploadFile");
+
 function backdropClickHandler() {
   backdrop.style.display = "none";
   sideDrawer.classList.remove("open");
@@ -14,3 +17,21 @@ function menuToggleClickHandler() {
 
 backdrop.addEventListener("click", backdropClickHandler);
 menuToggle.addEventListener("click", menuToggleClickHandler);
+
+// dragover and dragenter events need to have 'preventDefault' called
+// in order for the 'drop' event to register.
+// dropContainer.ondragover = dropContainer.ondragenter = function (evt) {
+//   evt.preventDefault();
+// };
+
+// dropContainer.ondrop = function (evt) {
+//   evt.preventDefault();
+//   // pretty simple -- but not for IE :(
+//   uploadFile.files = evt.dataTransfer.files;
+
+//   // If you want to use some of the dropped files
+//   const dT = new DataTransfer();
+//   dT.items.add(evt.dataTransfer.files[0]);
+//   dT.items.add(evt.dataTransfer.files[3]);
+//   uploadFile.files = dT.files;
+// };
