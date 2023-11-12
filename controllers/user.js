3cedc156e7b65f7d8d26fe6d6f8dce46d5ae80c2
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-11-12 21:28:26 +0100

    updated user controller

--- controllers/user.js
+++ controllers/user.js
@@ -11,3 +11,34 @@ exports.getMyBooks = (req, res, next) => {
     pageTitle: "My books",
   });
 };
+
+exports.postBook = (req, res, next) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const authorName = req.body.authorName;
+  const uploadFile = req.file;
+  const publicationDate = req.body.publicationDate;
+  const rating = req.body.rating;
+  const pages = req.body.pages;
+  const language = req.body.language;
+  const readingAge = req.body.readingAge;
+  const isbn13 = req.body.isbn13;
+
+  console.log({
+    title,
+    description,
+    authorName,
+    publicationDate,
+    rating,
+    pages,
+    language,
+    readingAge,
+    isbn13,
+    uploadFile,
+  });
+
+  console.log("BODY ");
+  console.log(req.body);
+
+  res.redirect("/");
+};
