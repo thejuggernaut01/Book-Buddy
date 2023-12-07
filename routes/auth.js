8a6e08b52f149832c1593d2fb18df37cf690d8cd
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-12-07 22:29:01 +0100

    added validation to signup route

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { body } = require("express-validator");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -10,7 +12,34 @@ router.get("/signup", authController.getSignup);
 
 router.post("/login", authController.postLogin);
 
-router.post("/signup", authController.postSignUp);
+router.post(
+  "/signup",
+  [
+    body("firstName").notEmpty().trim().escape(),
+    body("lastName").notEmpty().trim().escape(),
+    body("email")
+      .notEmpty()
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value) => {
+        return User.findOne(value).then((user) => {
+          if (user) {
+            return Promise.reject("Email exist already, pick a different one.");
+          }
+        });
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 6 characters long."
+    )
+      .notEmpty()
+      .trim()
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .escape(),
+  ],
+  authController.postSignUp
+);
 
 router.post("/logout", authController.postLogout);
 
