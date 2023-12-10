8cb1b56284caa35bdf22d9c3cb5e2c58725ebe80
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-12-10 21:46:27 +0100

    updated postSignup to send verification when a user successfully sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
-const User = require("../models/user");
-const { getDB } = require("../utils/database");
 const bcryptjs = require("bcryptjs");
 
+const User = require("../models/user");
+
+const { getDB } = require("../utils/database");
 const { validationResult } = require("express-validator");
-const getMsgForPath = require("../utils/helper");
+const { getMsgForPath, signUpVerification } = require("../utils/helper");
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
@@ -39,6 +40,13 @@ exports.getSignup = (req, res, next) => {
   });
 };
 
+exports.verifyEmail = (req, res, next) => {
+  res.render("auth/verify-email", {
+    path: "/verify-email",
+    pageTitle: "Verify your email!",
+  });
+};
+
 exports.postLogin = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -66,7 +74,7 @@ exports.postLogin = async (req, res, next) => {
           if (doMatch) {
             req.session.isLoggedIn = true;
             req.session.user = user;
-            return req.session.save((err) => {
+            return req.session.save(async (err) => {
               res.redirect("/");
             });
           }
@@ -83,7 +91,6 @@ exports.postLogin = async (req, res, next) => {
           });
         })
         .catch((err) => {
-          console.log(err);
           res.redirect("/login");
         });
     })
@@ -95,17 +102,19 @@ exports.postLogin = async (req, res, next) => {
     });
 };
 
-exports.postSignUp = (req, res, next) => {
+exports.postSignUp = async (req, res, next) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
   const password = req.body.password;
   const age = req.body.age;
   const favorite = { books: [] };
+  const verified = false;
 
   const errors = validationResult(req);
   const errorArray = errors.array();
 
+  // check if there's validation error
   if (!errors.isEmpty()) {
     return res.status(422).render("auth/signup", {
       path: "/signup",
@@ -125,37 +134,72 @@ exports.postSignUp = (req, res, next) => {
     });
   }
 
-  const db = getDB();
-  db.collection("users")
-    .findOne({ email: email })
-    .then((result) => {
-      if (result) {
+  // generate random number (size = 32)
+  crypto.randomBytes(32, async (err, buffer) => {
+    // checking an error occured after generating random values
+    if (err) {
+      return res.redirect("/signup");
+    }
+
+    try {
+      // query db whether user already exists
+      const user = await User.findOne(email);
+
+      // return to sign up page if user exists
+      if (user) {
         return res.redirect("/signup");
       }
 
-      bcryptjs
-        .hash(password, 12)
-        .then((hashPassword) => {
-          const user = new User(
-            firstName,
-            lastName,
-            email,
-            hashPassword,
-            +age,
-            favorite
-          );
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect("/login");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      // converted the randomBytes buffer to hex string
+      const token = buffer.toString("hex");
+
+      // hash user password
+      const hashedPassword = await bcryptjs.hash(password, 12);
+
+      // save user
+      const newUser = new User(
+        firstName,
+        lastName,
+        email,
+        hashedPassword,
+        +age,
+        favorite,
+        verified
+      );
+      await newUser.save();
+
+      // mail options for nodemailer
+      const mailOptions = {
+        from: '"<Book Buddy>" bookbuddy@gmail.com',
+        to: email,
+        subject: "Verify Your Account on Book Buddy",
+        html: `
+          <main>
+            <p>Dear ${firstName},</p>
+            <p>Welcome to Book Buddy! 📚✨ To ensure the security of your account, we kindly ask you to verify your email address.</p>
+
+            <p>Please click on the following link to complete the verification process: <a href="http://localhost:3030/verify-email/${token}">Verification Link</a></p>
+
+            <p>Note: This link is valid for the next 30 minutes. If you don't verify your account within this timeframe, you may need to request a new verification email.</p>
+
+            <p>If you did not sign up for Book Buddy, please ignore this email.</p>
+
+            <p>Thank you for being a part of our reading community!</p>
+
+            <p>Best,<br>
+              [Your Book Sharing Platform] Team</p>
+          </main>
+    `,
+      };
+
+      // send verification email
+      await signUpVerification(res, next, email, mailOptions, token);
+    } catch (err) {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    }
+  });
 };
 
 exports.postLogout = (req, res, next) => {
