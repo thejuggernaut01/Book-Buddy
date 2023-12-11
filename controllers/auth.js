251ef232dd67f469121b54df2de43099d07a1e21
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-12-11 09:59:37 +0100

    updated signup, login, verifiedUser controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ const User = require("../models/user");
 
 const { getDB } = require("../utils/database");
 const { validationResult } = require("express-validator");
-const { getMsgForPath, signUpVerification } = require("../utils/helper");
+const { getMsgForPath, sendEmail } = require("../utils/helper");
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
@@ -74,6 +74,27 @@ exports.verifiedUser = async (req, res, next) => {
     }
   );
 
+  // mail options for nodemailer
+  const mailOptions = {
+    from: '"Book Buddy" bookbuddy@gmail.com',
+    to: user.email,
+    subject: "Welcome to Book Buddy",
+    html: `
+      <main>
+        <p>Dear ${user.firstName},</p>
+
+        <p>Welcome to Book Buddy! 📚✨ We're thrilled to have you join our community of book lovers.</p>
+
+        <p>Explore your personalized dashboard, share your favorite reads, and join discussions with fellow readers. Stay tuned for exciting updates!</p>
+
+        <p>Need assistance? We're here for you. Happy reading!</p>
+
+        <p>Best,<br>
+          Book Buddy Team</p>
+      </main>
+    `,
+  };
+
   res.render("auth/verified", {
     path: "/verified",
     pageTitle: "You're account has been verified!",
@@ -84,67 +105,71 @@ exports.postLogin = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne(email)
-    .then((user) => {
-      if (!user) {
-        return res.status(401).render("auth/login", {
-          path: "/login",
-          pageTitle: "Login into your account!",
-          errorMessage: "Invalid email or password.",
-          oldInput: {
-            email: email,
-            password: password,
-          },
-          validationErrors: [],
-        });
-      }
+  try {
+    // query db to check for user
+    const user = await User.findOne(email);
 
-      if (!user.verified) {
-        return res.status(401).render("auth/login", {
-          path: "/login",
-          pageTitle: "Login into your account!",
-          errorMessage:
-            "Your account hasn't been verified, check your email and verify.",
-          oldInput: {
-            email: email,
-            password: password,
-          },
-          validationErrors: [],
-        });
-      }
+    // check if user does not exist
+    // render the page with appropriate error message
+    if (!user) {
+      return res.status(401).render("auth/login", {
+        path: "/login",
+        pageTitle: "Login into your account!",
+        errorMessage: "Invalid email or password.",
+        oldInput: {
+          email: email,
+          password: password,
+        },
+        validationErrors: [],
+      });
+    }
 
-      bcryptjs
-        .compare(password, user.password)
-        .then((doMatch) => {
-          if (doMatch) {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            return req.session.save(async (err) => {
-              res.redirect("/");
-            });
-          }
+    // check if user is not verified
+    // render the page with appropriate error message
+    if (!user.verified) {
+      return res.status(401).render("auth/login", {
+        path: "/login",
+        pageTitle: "Login into your account!",
+        errorMessage:
+          "Your account hasn't been verified, check your email and verify.",
+        oldInput: {
+          email: email,
+          password: password,
+        },
+        validationErrors: [],
+      });
+    }
 
-          return res.status(401).render("auth/login", {
-            path: "/login",
-            pageTitle: "Login into your account!",
-            errorMessage: "Invalid email or password.",
-            oldInput: {
-              email: email,
-              password: password,
-            },
-            validationErrors: [],
-          });
-        })
-        .catch((err) => {
-          res.redirect("/login");
-        });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+    // compare inputted password with hashed password stored in db
+    const doMatch = await bcryptjs.compare(password, user.password);
+
+    // if password match, user is authenticated
+    if (doMatch) {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      return req.session.save(async (err) => {
+        res.redirect("/");
+      });
+    }
+
+    // if password does not match
+    // render the page with appropriate error message
+    return res.status(401).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login into your account!",
+      errorMessage: "Invalid email or password.",
+      oldInput: {
+        email: email,
+        password: password,
+      },
+      validationErrors: [],
     });
+  } catch (error) {
+    if (!error.statusCode) {
+      err.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
 exports.postSignUp = async (req, res, next) => {
@@ -215,7 +240,7 @@ exports.postSignUp = async (req, res, next) => {
 
       // mail options for nodemailer
       const mailOptions = {
-        from: '"<Book Buddy>" bookbuddy@gmail.com',
+        from: '"Book Buddy" bookbuddy@gmail.com',
         to: email,
         subject: "Verify Your Account on Book Buddy",
         html: `
@@ -232,13 +257,13 @@ exports.postSignUp = async (req, res, next) => {
             <p>Thank you for being a part of our reading community!</p>
 
             <p>Best,<br>
-              [Your Book Sharing Platform] Team</p>
+              Book Buddy Team</p>
           </main>
     `,
       };
 
       // send verification email
-      await signUpVerification(res, next, email, mailOptions, token);
+      await sendEmail(res, next, email, mailOptions, token, "signUpEmail");
     } catch (err) {
       const error = new Error(err);
       error.httpStatusCode = 500;
